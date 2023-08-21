d68bcaaf6379498fae45689d783cffdf0dcd5d3d
Donte Ladatto <donte279@gmail.com>
2023-08-20 21:11:31 -0500

    set up initial pages and routes

--- front-end/src/routes/Routers.jsx
+++ front-end/src/routes/Routers.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
+import Contact from '../pages/Contact'
+import { Routes, Route } from 'react-router-dom'
 
 const Routers = () => {
   return (
-    <div>Routers</div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/home' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/contact' element={<Contact />} />
+    </Routes>
   )
 }
 
